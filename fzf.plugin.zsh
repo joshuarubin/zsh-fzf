948d9f361b55f5f4a02e021a7d72613cd867ef15
Joshua Rubin <jrubin@zvelo.com>
2016-10-05 11:52:51 -0600

    use fzf in home dir

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -1,31 +1,15 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
+if [ -d "$HOME/.fzf" ]; then
+  path[1,0]="$HOME/.fzf/bin"
+  manpath[1,0]="$HOME/.fzf/man"
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
-fi
+  source "$HOME/.fzf/shell/completion.zsh"
+  source "$HOME/.fzf/shell/key-bindings.zsh"
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+  _fzf_compgen_path() {
+    ag -g "" "$1"
+  }
 
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
-# Use ag instead of the default find command for listing candidates.
-# - The first argument to the function is the base path to start traversal
-# - Note that ag only lists files not directories
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  ag -g "" "$1"
-}
-
-if [ -f "$HOME/.fzf.conf" ]; then
-  source "$HOME/.fzf.conf"
+  if [ -f "$HOME/.fzf.conf" ]; then
+    source "$HOME/.fzf.conf"
+  fi
 fi
