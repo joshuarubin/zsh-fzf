ef61f74e722f3d4330e49cc28556e76530627a7d
Joshua Rubin <jrubin@zvelo.com>
2016-11-23 19:44:58 -0700

    add execution support to fzf shell history

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -12,4 +12,33 @@ if [ -d "$HOME/.fzf" ]; then
   if [ -f "$HOME/.fzf.conf" ]; then
     source "$HOME/.fzf.conf"
   fi
+
+  fzf-history-widget-accept() {
+    fzf-history-widget
+    zle accept-line
+  }
+  zle     -N     fzf-history-widget-accept
+  bindkey '^X^R' fzf-history-widget-accept
+
+  fzf-history-widget() {
+    local selected num
+    setopt localoptions noglobsubst pipefail 2> /dev/null
+    selected=( $(fc -l 1 | eval "$(__fzfcmd) +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r --expect=ctrl-x $FZF_CTRL_R_OPTS -q ${(q)LBUFFER}") )
+    local ret=$?
+    if [ -n "$selected" ]; then
+      local accept=0
+      if [[ $selected[1] = ctrl-x ]]; then
+        accept=1
+        shift selected
+      fi
+      num=$selected[1]
+      if [ -n "$num" ]; then
+        zle vi-fetch-history -n $num
+        [[ $accept = 1 ]] && zle accept-line
+      fi
+    fi
+    zle redisplay
+    typeset -f zle-line-init >/dev/null && zle zle-line-init
+    return $ret
+  }
 fi
